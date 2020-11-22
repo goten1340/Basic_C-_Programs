21254d39a0e27a336a665bd607839ce8cdc4674d
goten1340 <63024900+goten1340@users.noreply.github.com>
2020-11-22 15:37:37 -0800

    for
    tech

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            Int32 age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Answer \"true\" or \"false\".");
+            Boolean gotcha = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How mant speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualify?");
+            Boolean isIt = age > 15 && gotcha == false && speedingTickets <= 3;
+            Console.WriteLine(isIt);
+            Console.ReadLine();
+
+
+        }
+    }
+}
