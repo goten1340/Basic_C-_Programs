c89253f4382a94765fe26994338735157f182296
goten1340 <63024900+goten1340@users.noreply.github.com>
2020-11-23 17:40:29 -0800

    for tech
    for tech

---
+++ StringFunctions Assignment/StringFunctions Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello " + "how" + " are you?";
+            string upper = text.ToUpper();
+
+            StringBuilder sb = new StringBuilder("Hello World");
+            sb.Append(" This is my place.");
+            sb.Append(" What i learn is to give back to my community.");
+            sb.Append(" What else should I say?");
+            sb.Append(" Thank you for listening.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Ricardo";
+            //string quote = "The man said, \\Hello\", Ricardo, \n Hello on a new line. \n \t Hello on a new tab.";
+            //string fileName = @"C:\Users\'ricar";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToLower();
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Ricardo");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
