f424f9ea8252a54a7a19845341f1578829f7659a
goten1340 <63024900+goten1340@users.noreply.github.com>
2020-11-24 19:53:26 -0800

    tech
    tech academy

--- ArraysAndListsAssignment/ArraysAndListsAssignment/Program.cs
+++ ArraysAndListsAssignment/ArraysAndListsAssignment/Program.cs
@@ -13,84 +13,93 @@ namespace ArraysAndListsAssignment
 
 
             {
-                string[] stringArray = { "Hello", "Ricardo", "Welcome" };
+                //1.Create a one-dimensional Array of strings.Ask the user to select an index of the Array and then display the string at that index on the screen.
+                //3.Add in a message that displays when the user selects an index that doesn’t exist.
 
-
-
-
-                int[] numArray = { 4, 6, 8, 10, 12 };
-
-
-
+                string[] stringArray = { "Hello", "Ricardo", "Welcome", "hi" };
 
                 try
                 {
                     Console.WriteLine("Please select string index from 0-2.");
-                    string pickString = (Console.ReadLine());
-                    int hi = Convert.ToInt32(pickString);
-                    if (hi > 2)
-                    {
-                        Console.WriteLine("Invalid Input");
-                    }
-                    else
-                    {
-                        Console.WriteLine(stringArray[hi]);
-
-                    }
-                    Console.WriteLine("Please select integer index from 0-4.");
-                    string pickInt = (Console.ReadLine());
-
-                    int hello = Convert.ToInt32(pickInt);
-                    if (hello > 4)
-                    {
-                        Console.WriteLine("This is an invalid input");
-                    }
-                    else
-                    {
-                        Console.WriteLine(numArray[hello]);
-                    }
-
-
+                    int pickString = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The string you selected is  " + stringArray[pickString]);
                 }
                 catch (FormatException)
                 {
                     Console.WriteLine("You did not type an input.");
                 }
-
-
-                //if (pickString == "")
-                //    Console.WriteLine("You didnt pick an index.");
-                //if (pickString > 2)
-                //    Console.WriteLine("Invalid input.");
-
-                List<string> hey = new List<string>
+                catch
                 {
-                    "Auto", "whatsUp", "CanI"
-                };
+                    Console.WriteLine("Index does not exist");
+                }
+
+
+                //2.Create a one-dimensional Array of integers.Ask the user to select an index of the Array and then display the integer at that index on the screen.
+                //3.Add in a message that displays when the user selects an index that doesn’t exist.
+
+                int[] numArray = { 4, 6, 8, 10, 12 };
+                try {
+                    Console.WriteLine("Please select number index from 0-4");
+                    int pickNumber = Convert.ToInt32(Console.ReadLine());
+                    
+
+                    if (pickNumber > 4)
+                    
+                    {
+                        Console.WriteLine("Index does not exist.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number you selected is " + numArray[pickNumber]);
+
+                    }
+                    //if (pickString == "")
+                    //    Console.WriteLine("You didnt pick an index.");
+                    //if (pickString > 2)
+                    //    Console.WriteLine("Invalid input.");
+
+                    //4.Create a list of strings. Ask the user to select an index of the list and then display the content at that index on the screen.
+                    //3.Add in a message that displays when the user selects an index that doesn’t exist.
+
+                    
+                }
+                
+                catch (FormatException) { 
+                Console.WriteLine("You gave no input.");
+                
+                }
+
+                List<string> hello = new List<string>() { "Auto", "whatsUp", "CanI" };
 
                 try
                 {
                     Console.WriteLine("Please select a index from 0-2");
                     int pickANum = Convert.ToInt32(Console.ReadLine());
-                    if (pickANum > 2)
+                    //Console.WriteLine("The index number you selected contains the content " + hello);
+                    if (pickANum < 0 || pickANum > 2)
                     {
-                        Console.WriteLine("This is an invalid input.");
+                        Console.WriteLine("This is an invalid input");
+
                     }
                     else
                     {
-                        Console.WriteLine(hey[pickANum]);
+                        Console.WriteLine(hello[pickANum]);
                     }
                 }
                 catch (FormatException)
                 {
                     Console.WriteLine("You gave no input");
                 }
+                Console.ReadLine();
+                    
+                
+
+
+
+           
+
+
             }
-
-
-            Console.ReadLine();
-
-
         }
     }
 }
