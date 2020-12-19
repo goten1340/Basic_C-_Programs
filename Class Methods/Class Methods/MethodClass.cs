1753c756923d7849fd0326625985da3ffbbbe3ad
goten1340 <63024900+goten1340@users.noreply.github.com>
2020-12-19 14:56:00 -0800

    completed the console app asssignment

---
+++ Class Methods/Class Methods/MethodClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods
+{
+    class MethodClass
+    {
+         public int AddEmUp(int x)
+        {
+            int results = x + 7;
+            return results;
+        }
+        public int Minus(int c)
+        {
+            int results1 = c - 4;
+            return results1;
+        }
+        public int division(int d)
+        {
+            int results2 = d / 5;
+            return results2;
+        }
+    }
+}
---
+++ Class Methods/Class Methods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you wish to perform the operation on?");
+                int numberPicked = Convert.ToInt32(Console.ReadLine());
+            MethodClass obj1 = new MethodClass();
+            Console.WriteLine(obj1.AddEmUp(numberPicked));
+            Console.WriteLine(obj1.Minus(numberPicked));
+            Console.WriteLine(obj1.division(numberPicked));
+            Console.ReadLine();
+
+        }
+    }
+}
