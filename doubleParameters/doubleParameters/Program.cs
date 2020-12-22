a7a4c1f7ad472b6f074bd7849f5af630a9d29794
goten1340 <63024900+goten1340@users.noreply.github.com>
2020-12-22 15:31:21 -0800

    pushing doubleParameter assigment

---
+++ doubleParameters/doubleParameters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doubleParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            optionalPara obj = new optionalPara();
+            Console.WriteLine(obj.doublePara(5));
+            Console.WriteLine("Choose first number");
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You may input a second number if you want");
+            string Number2 = Console.ReadLine();
+            if (Number2 == "")
+            {                
+                Console.WriteLine(obj.optionalSecondNumber(Number1));
+                
+            }
+            else
+            {
+                int conversion = Convert.ToInt32(Number2);
+                Console.WriteLine(obj.optionalSecondNumber(Number1, conversion));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ doubleParameters/doubleParameters/optionalPara.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doubleParameters
+{
+    class optionalPara
+    {
+        public int doublePara(int a, int b = 3)
+        {
+            int results = 5 - a - b;
+            return results;
+
+        }
+        public int optionalSecondNumber(int s , int f = 4)
+        {
+            int results2 = 6 - s - f;
+            return results2;
+        }
+    }
+}
