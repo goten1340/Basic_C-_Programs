08144cd5eebea89f6ac7f0d770974344c8d788e0
goten1340 <63024900+goten1340@users.noreply.github.com>
2020-12-23 11:44:21 -0800

    completed the void assignement
    completed the void assigment

---
+++ voidSpace/voidSpace/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            moreParameters obj1 = new moreParameters();
+            obj1.takeIn(4, 7);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ voidSpace/voidSpace/moreParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidSpace
+{
+    class moreParameters
+    {
+        public void takeIn(int f, int l)
+            {
+                Console.WriteLine( f - 7 + l);
+            
+            }
+    }
+}
