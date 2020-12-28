06e045deb03126fa2ec05081d70aea97507206ad
goten1340 <63024900+goten1340@users.noreply.github.com>
2020-12-28 15:31:51 -0800

    finished assignment

---
+++ classProperties/classProperties/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classProperties
+{
+   public class Person
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name:" + FirstName + " " + LastName);
+        }
+    }
+    class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ classProperties/classProperties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee obj1 = new Employee();
+            obj1.FirstName = "Sample";
+            obj1.SayName();
+            obj1.LastName = "Student";
+            Console.ReadLine();
+
+            
+        }
+    }
+}
