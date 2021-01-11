4ee782b15531610faa4c5af6aea88509413e8fd9
goten1340 <63024900+goten1340@users.noreply.github.com>
2021-01-11 15:27:11 -0800

    logs assignment

---
+++ logs/logs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace logs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number?");
+            string results = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Owner\source\repos\logs\logs\logs.txt", results);
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Owner\source\repos\logs\logs\logs.txt"));
+            Console.ReadLine();
+        }
+    }
+}
