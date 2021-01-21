ecbd80dff1a5dbc4c7d2f04c37652f266fafd5fa
goten1340 <63024900+goten1340@users.noreply.github.com>
2021-01-20 20:22:39 -0800

    for instrcuotr

---
+++ NewsLetterAppMVC/NewsLetterAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewsLetterAppMVC.Models;
+using NewsLetterAppMVC.ViewModels;
+
+namespace NewsLetterAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+               using (NewsletterEntities db = new NewsletterEntities())
+                {
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
+
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }
+
+                return View("Success");
+            }
+        }
+
+       public ActionResult Admin()
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                var signups = db.SignUps;
+                var signUpVms = new List<SignUpVm>();
+                foreach (var signup in signups)
+                {
+                    var signupvm = new SignUpVm();
+                    signupvm.FirstName = signup.FirstName;
+                    signupvm.LastName = signup.LastName;
+                    signupvm.EmailAddress = signup.EmailAddress;
+                    signUpVms.Add(signupvm);
+                }
+                return View(signUpVms);
+            }
+            
+            
+        }
+    }
+}
